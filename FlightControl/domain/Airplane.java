6c08b9bb7a1a28ec9e273b680d6f9e2a2873e698
Damien R <damien.rembert@gmail.com>
2022-03-02 16:36:10 +0000

    added plane

--- FlightControl/domain/Airplane.java
+++ FlightControl/domain/Airplane.java
@@ -1,5 +1,22 @@
 package FlightControl.domain;
 
+import java.lang.reflect.Constructor;
+
 public class Airplane {
+    private String name;
+    private int capacity;
+
+    public Airplane(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    };
+
+    public String getName() {
+        return this.name;
+    };
+
+    public int getCapacity() {
+        return this.capacity;
+    }
     
 }
